31be28a094101744cbe9d0a0e2e1fd56d3ccf268
Juan <66238416+n3mphis@users.noreply.github.com>
2025-01-07 15:54:47 -0300

    I got it to work but probably will re-do it

--- javascript.js
+++ javascript.js
@@ -1,36 +1,102 @@
-/* SECTION FOR SCORE */
 let humanScore = 0;
 let computerScore = 0;
 
-/* SECTION FOR STRINGS VARIABLE */
-let r = "Rock";
-let p = "Paper";
-let s = "Scissors";
-
-let input = prompt("Please choose Rock, Paper or Scissors");
-/* RANDOM NUMBER */
-let randomNumber = Math.floor(Math.random() * 100) + 1;
-
-/* SECTION FOR FUNCTIONS */
 
 function getComputerChoice() {
-    if (randomNumber <= 40) {
-        return r;
-    } else if (randomNumber <= 70) {
-        return s;
+    let randomNumber = Math.random();
+
+    if (randomNumber <= 0.33) {
+        return "paper";
+    } else if (randomNumber > 0.66) {
+        return "rock";
     } else {
-        return p;
+        return "scissors";
     }
 }
 
 function getHumanChoice() {
-    if (input === r || input === p || input === s) {
-        return alert("You chose " + input);
-    } else {
-        return alert(input + " is not a valid option");
+    let input = prompt("Choose your weapon");
+    input = input.toLowerCase();
+
+    return input;
+}
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice == "rock") 
+        switch(computerChoice) {
+    case "paper":
+        {
+            alert("You lost. Paper beates Rock");
+            ++computerScore;
+            break;
+        }
+    case "scissors":
+        {
+            alert("You win! Rock beats Scissors");
+            ++humanScore;
+            break;
+        }
+        default:
+            alert("It's a draw");
+            break;
+        }
+    else if (humanChoice == "paper")
+        switch(computerChoice) 
+    {
+        case "scissors":
+            {
+                alert("You lose. Scissors beats Rock");
+                ++computerScore;
+                break;
+            }
+        case "rock":
+            {
+                alert("You win! Paper beates Rock");
+                ++humanScore;
+                break;
+            }
+            default:
+                alert("It's a draw");
+                break;
+    }
+    else if (humanChoice == "scissors")
+        switch(computerChoice)
+    {
+        case "rock":
+            {
+                alert("You lose. Rock beats Scissors");
+                ++computerScore;
+                break;
+            }
+        case "paper":
+            {
+                alert("You win! Scissors beats Paper");
+                ++humanScore;
+                break;
+            }
+        default:
+            alert("It's a draw");
+            break;
+    }
+    else {
+        alert("Invalid Choice");
     }
 }
-/* SECTION FOR CONSOLE LOG */
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+function playGame() {
+    for (let i = 0; i < 5; i++) {
+        playRound(getHumanChoice(), getComputerChoice());
+    }
+    if (humanScore > computerScore) {
+       return alert("CONGRATULATIONS YOU WON!!");
+    } else if (humanScore < computerScore) {
+        return alert("You Lost. Refresh to play again");
+    } else {
+        return alert("That's awkward, a TIE");
+    }
+}
+
+playGame();
+console.log("Results:");
+console.log("Human: " + humanScore);
+console.log("Computer: " + computerScore);
